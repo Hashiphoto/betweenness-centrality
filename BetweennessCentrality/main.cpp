d151868a8aee9fb5104915c314959595601e2dc2
Trent Hashimoto <t.hashiphoto@gmail.com>
2019-02-24 14:47:00 -0800

    Initial commit

---
+++ BetweennessCentrality/main.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// Read in the data
+		// Iterate through every 
+
+	// Coarsen Graph
+	/*
+		Input: G0 – initial graph, num hubs – number of vertices, which are considered as hubs
+		Output: G1 – coarsened graph
+		1 G1 ← G0;
+		2 hubs list ← get highest centrality values(G0, num hubs);
+		3 foreach hub in hubs list do
+		4 vertices del ← get neighbors(G0, hub);
+		5 foreach vertex in vertices del do
+		6 vertices connect ← vertices connect ∪ get neighbors(G0, vertex);
+		7 end
+		8 foreach vertex in vertices del do
+		9 remove vertex(G1, vertex);
+		10 end
+		11 foreach vertex in vertices connect do
+		12 add edge(G1, hub, vertex);
+		13 end
+		14 end
+		15 return G1;
+	*/
+
+	// Calculate Betweenness Centrality of Coarsened Graph
+
+	return 0;
+}
+
